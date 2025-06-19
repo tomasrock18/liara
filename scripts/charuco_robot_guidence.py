75c94e8b41c76d2dd620436e426a40886773b788
kovs <ovs123321@gmail.com>
2025-06-19 21:45:17 +0300

    Добавил калибровку

--- scripts/charuco_robot_guidence.py
+++ scripts/charuco_robot_guidence.py
@@ -5,6 +5,10 @@ import numpy
 #####################################################################
 CAMERA_ID = 1
 CAMERA_FRAME_SIZE = (1920, 1080)
+BOARD_SIZE = (7, 9)
+BOARD_MARKER_LENGTH_M = 0.025
+BOARD_SQUARE_LENGTH_M = 0.03
+BOARD_ARUCO_DICTIONARY = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_ARUCO_ORIGINAL)
 #####################################################################
 
 
@@ -60,4 +64,91 @@ while True:
     cv2.imshow(camera_setting_window_name, frame)
     if cv2.waitKey(1) & 0xFF == ord("\r"):
         cv2.destroyAllWindows()
+        break
+#####################################################################
+
+# 3) Выполняем калибровку камеры
+#####################################################################
+# Создание объекта доски ChAruco
+calibration_board = cv2.aruco.CharucoBoard(
+    size=BOARD_SIZE,
+    markerLength=BOARD_MARKER_LENGTH_M,
+    squareLength=BOARD_SQUARE_LENGTH_M,
+    dictionary=BOARD_ARUCO_DICTIONARY,
+)
+
+# Инициализация массивов данных для калибровки
+all_charuco_corners = []
+all_charuco_ids = []
+
+# Инициализация детектора параметров калибровки
+detector = cv2.aruco.CharucoDetector(calibration_board)
+
+# Инициализация массива изображений для калибровки
+calibration_images = []
+
+# Настройка окна
+window_name = "Initial Calibration"
+cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+
+# Запуск цикла работы камеры
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        raise Exception("Камера не вернула картинку")
+
+    # Переводим в серое
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Ищем углы
+    charuco_corners, charuco_ids, _, _ = detector.detectBoard(grey)
+
+    # Отрисовка ну и обработка + разметка
+    if None not in (charuco_corners, charuco_ids):
+        cv2.aruco.drawDetectedCornersCharuco(frame, charuco_corners, charuco_ids)
+
+    cv2.putText(
+        frame,
+        f"Frames for calibration: {len(all_charuco_corners)}",
+        (10, 30),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        1
+    )
+    cv2.putText(
+        frame,
+        "Press 'a' to add frame for calibration",
+        (10, 60),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        2
+    )
+    cv2.putText(
+        frame,
+        "Press 'Enter' to start calibration",
+        (10, 90),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        2
+    )
+
+    cv2.imshow(window_name, frame)
+    if cv2.waitKey(1) & 0xFF == ord("a"):
+        all_charuco_corners.append(charuco_corners)
+        all_charuco_ids.append(charuco_ids)
+    elif cv2.waitKey(1) & 0xFF == ord("\r"):
+        projection_error_calibration, camera_matrix, dist_coeffs, _, _ = cv2.aruco.calibrateCameraCharuco(
+            all_charuco_corners,
+            all_charuco_ids,
+            calibration_board,
+            frame.shape,
+            cameraMatrix=None,
+            distCoeffs=None,
+        )
+        cv2.destroyAllWindows()
+        break
+
 #####################################################################
